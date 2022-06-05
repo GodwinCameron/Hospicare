401468ec6f6c2607f4b9f54c0280c69bfa91cd65
Godwin Cameron <200109@virtualwindow.co.za>
2022-06-05 22:18:24 +0200

    dash frontend style progress

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -3,8 +3,42 @@ import { Container, Col, Row } from "react-bootstrap";
 
 const Dash = () => {
     return (<>
-            <Col md={1} className="test">
-            </Col>
+        <Col md={11} className="dash-main-div">
+            <div className="dash-left-col">
+                <h1>Welcome Back Dr. !</h1>
+                <h6>Let's see what your day looks like...</h6>
+                <Row>
+                    <Col md={8} className="dash-appointments-summary">
+                        <div className="dash-appointment-summary-item"></div>
+                        <div className="dash-appointment-summary-item"></div>
+                        <div className="dash-appointment-summary-item"></div>
+                        <div className="dash-appointment-summary-item"></div>
+                        <div className="dash-appointment-summary-item"></div>
+                        <div className="dash-appointment-summary-item"></div>
+                    </Col>
+                    <Col className="dash-breakdown">
+                        <h4>Breakdown of July</h4>
+                        <div className="chart"></div>
+                        <div className="dash-breakdown-info"></div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <h4>Schedule:</h4>
+                    <Col md={6} className="dash-schedule-week"></Col>
+                    <Col md={1}></Col>
+                    <Col className="dash-schedule-month"></Col>
+                </Row>
+            </div> {/*Left Col end*/}
+
+            <div className="dash-right-col">
+                <div className="dash-menu-icon-holder">
+                    <div className="dash-icons dash-to-profile-menu-icon"></div>
+                    <div className="dash-icons dash-settings-icon"></div>
+                </div>
+                <div className="dash-profile-pic"></div>
+            </div> {/*Right Col end*/}
+        </Col>
     </>)
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import Dash from "./Dash";
 
 const Nav = () => {
     return (<>
-        <Row>
             <Col md={1} className="nav-main-div">
                 <div className="nav-logo"></div>
                 <div className="nav-item-separator"></div>
@@ -11,9 +11,7 @@ const Nav = () => {
                 <div className="nav-item nav-tasks" id="navTasksIcon"></div>
                 <div className="nav-item nav-cal" id="navCalIcon"></div>
                 <div className="nav-item nav-appoint" id="navppointIcon"></div>
-
             </Col>
-        </Row>
     </>)
 }
 
