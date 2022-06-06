ce27112ad8f598be1e29b6d32b1dd2c8d1872be1
Godwin Cameron <200109@virtualwindow.co.za>
2022-06-06 02:05:28 +0200

    Revert "front end dash progress"
    This reverts commit 00db48d37349efe3604d857d9d3163753c2ce5af.

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import DashAppointSumItem from "./DashAppointSumItem";
+import { Container, Col, Row } from "react-bootstrap";
 
 const Dash = () => {
     return (<>
@@ -10,7 +9,7 @@ const Dash = () => {
                 <h6>Let's see what your day looks like...</h6>
                 <Row>
                     <Col md={8} className="dash-appointments-summary">
-                        <DashAppointSumItem />
+                        <div className="dash-appointment-summary-item"></div>
                         <div className="dash-appointment-summary-item"></div>
                         <div className="dash-appointment-summary-item"></div>
                         <div className="dash-appointment-summary-item"></div>
@@ -39,26 +38,7 @@ const Dash = () => {
                         <div className="dash-icons dash-to-profile-menu-icon"></div>
                         <div className="dash-icons dash-settings-icon"></div>
                     </div>
-                    <div className="rank"></div>
                     <div className="dash-profile-pic"></div>
-                    <h2>Dr. DJ Cat</h2>
-                    <h5>Surgeon</h5>
-                    <div className="dash-see-profile">
-                        <p>See Profile</p>
-                    </div>
-                    <h5>Working Hours:</h5>
-                    <div className="dash-hours">
-                        <div className="dash-working-hours-start">
-                            <p>Work Start</p>
-                            <p1>09:00 AM</p1>
-                        </div>
-                        <div className="dash-working-hours-end">
-                            <p>Work End</p>
-                            <p1>05:00 PM</p1>
-                        </div>
-                    </div>
-                    <h1>Centurion, Pretoria</h1>
-                    <h5>South Africa, 22°C <br></br>June 4 Sat.<br></br>14:32</h5>
                 </div>
             </div> {/*Right Col end*/}
         </Col>
